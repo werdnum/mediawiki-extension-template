3d28774f5850f2794fdbceaee7f2834712052a6c
Andrew Garrett <agarrett@wikimedia.org>
2015-03-12 10:00:57 +0100

    Initial commit

---
+++ ExtensionName.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * MediaWiki Extension: ExtensionName
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * This program is distributed WITHOUT ANY WARRANTY.
+ */
+
+/**
+ *
+ * @file
+ * @ingroup Extensions
+ * @author Your Name
+ */
+
+# Alert the user that this is not a valid entry point to MediaWiki if they try to access the special pages file directly.
+if ( !defined( 'MEDIAWIKI' ) ) {
+	echo <<<EOT
+To install this extension, put the following line in LocalSettings.php:
+require_once( "$IP/extensions/ExtensionName/ExtensionName.php" );
+EOT;
+	exit( 1 );
+}
+
+// Extension credits that will show up on Special:Version
+$wgExtensionCredits['specialpage'][] = array(
+	'path' => __FILE__,
+	'name' => 'ExtensionName',
+	'url' => 'URL to extension information',
+	'author' => array(
+		'Your Name',
+	),
+	'descriptionmsg' => 'extensionname-desc',
+);
+
+$dir = dirname( __FILE__ ) . '/';
+
+$wgMessagesDirs['ExtensionName'] =  . 'i18n';
